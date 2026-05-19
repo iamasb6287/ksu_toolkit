1e9384c6f00b91cc350b30c62887d62e8f4c7c9f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-19 09:19:07 +0800

    scripts/action: toggle sucompat status on benchmark

--- module/action.sh
+++ module/action.sh
@@ -7,7 +7,26 @@ PATH=/data/adb/ksu/bin:$PATH
 MODDIR="/data/adb/modules/ksu_toolkit"
 KSUDIR="/data/adb/ksu"
 
+# grab old setting
+original_sucompat_setting=$(ksud feature get 0 | grep "Value" | awk {'print $2'})
+original_adbroot_setting=$(ksud feature get 3 | grep "Value" | awk {'print $2'})
+original_sulog_setting=$(ksud feature get 2 | grep "Value" | awk {'print $2'})
+
+# disable sulog and adb root
+ksud feature set 2 0 > /dev/null 2>&1
+ksud feature set 3 0 > /dev/null 2>&1
+
+# enable sucompat
+ksud feature set 0 1 > /dev/null 2>&1
+
 "$MODDIR/toolkit" --bench
+
+ksud feature set 0 0 > /dev/null 2>&1
+"$MODDIR/toolkit" --bench
+
+ksud feature set 0 "$original_sucompat_setting" > /dev/null 2>&1
+ksud feature set 2 "$original_sulog_setting" > /dev/null 2>&1
+ksud feature set 3 "$original_adbroot_setting" > /dev/null 2>&1
 
 [ "$KSU_VER_CODE" -lt 32492 ] && sleep 20
 
