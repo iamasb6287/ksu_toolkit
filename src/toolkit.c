fed9435d87ad0ed67e81f71105331f653ef18c0d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-06-11 09:45:09 +0800

    toolkit: compatibility handling for KSU_IOCTL_GET_INFO
    https://github.com/tiann/KernelSU/pull/3455

--- src/toolkit.c
+++ src/toolkit.c
@@ -236,15 +236,25 @@ static int toolkit_main(long argc, char **argv, char **envp)
 		char buf_version[] = "ksuver: ??????\n";
 		char buf_flags[] = "flags: ??????\n";
 		char buf_features[] = "features: ??????\n";
+		char buf_uapiver[] = "uapi: ??????\n";
 
 		ksu_sys_reboot(KSU_INSTALL_MAGIC2, 0, (long)&fd);
 		if (!fd)
 			goto fail;
 
-		struct ksu_get_info_legacy_cmd *cmd = (struct ksu_get_info_legacy_cmd *)sp;
-		int ret = sys_ioctl(fd, KSU_IOCTL_GET_INFO_LEGACY, (long)cmd);
-		if (ret)
-			goto fail;
+		// handles ksu_get_info compat, legacy and one with uapi ver
+		// what we can do is to just prepare 4x u32 space (ksu_get_info_cmd) then act accordingly
+		struct ksu_get_info_cmd *cmd = (struct ksu_get_info_cmd *)sp;
+		int ret = sys_ioctl(fd, KSU_IOCTL_GET_INFO, (long)cmd);
+		if (ret) {
+			// if new ioctl fails, we try again, this time trying legacy
+			ret = sys_ioctl(fd, KSU_IOCTL_GET_INFO_LEGACY, (long)cmd);
+			if (ret)
+				goto fail;
+
+			dumb_itoa(0, 6, &buf_uapiver[6]);
+		} else
+			dumb_itoa(cmd->uapi_version, 6, &buf_uapiver[6]);			
 
 		dumb_itoa(cmd->version, 6, &buf_version[8]);
 		print_out(buf_version, sizeof(buf_version) - 1);
@@ -254,6 +264,8 @@ static int toolkit_main(long argc, char **argv, char **envp)
 
 		dumb_itoa(cmd->features, 6, &buf_features[10]);
 		print_out(buf_features, sizeof(buf_features) - 1);
+
+		print_out(buf_uapiver, sizeof(buf_uapiver) - 1);
 
 		return 0;
 
--- src/toolkit.h
+++ src/toolkit.h
@@ -38,6 +38,14 @@ struct ksu_get_info_legacy_cmd {
 };
 #define KSU_IOCTL_GET_INFO_LEGACY _IOC(_IOC_READ, 'K', 2, 0)
 
+struct ksu_get_info_cmd {
+	uint32_t version; /* Output: KERNEL_SU_VERSION */
+	uint32_t flags; /* Output: KSU_GET_INFO_FLAG_* bits */
+	uint32_t features; /* Output: max feature ID supported */
+	uint32_t uapi_version; /* Output: KERNEL_SU_UAPI_VERSION */
+};
+#define KSU_IOCTL_GET_INFO _IOR('K', 2, struct ksu_get_info_cmd)
+
 #define SULOG_ENTRY_MAX 250
 #define SULOG_BUFSIZ SULOG_ENTRY_MAX * (sizeof (struct sulog_entry))
 
